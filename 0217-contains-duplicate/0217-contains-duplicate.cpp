18ca3c29c2baba8cdbee672bf4b4ef8188c6bc95
cinematicbug <namanthebookworm@gmail.com>
2026-09-06 11:36:16 +0530

    0217-CONTAINS-DUPLICATE Stats: Time: 63 ms (75.61%), Space: 107.8 MB (76.19%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         unordered_set<int> ans;
+        ans.reserve(nums.size());
 
         for (int x : nums) {
             if (ans.count(x)) {
